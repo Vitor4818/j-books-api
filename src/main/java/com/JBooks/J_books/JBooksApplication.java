772724050c504780c353db8237ec911a081fcff1
Vitor4818 <vitorg.martins4818@gmail.com>
2025-03-23 16:11:08 -0300

    feat: Starting project

---
+++ src/main/java/com/JBooks/J_books/JBooksApplication.java
@@ -0,0 +1,13 @@
+package com.JBooks.J_books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JBooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/JBooks/J_books/JBooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JBooks.J_books;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JBooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
